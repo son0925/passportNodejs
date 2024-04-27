c079749edaa14f4e3538533770d5ce3b7a0b51c6
son0925 <shs00925@naver.com>
2024-04-27 15:17:31 +0900

    Restart mongoose, express Create

--- src/server.js
+++ src/server.js
@@ -1,59 +1,29 @@
-const express = require('express');
-const {default : mongoose} = require('mongoose');
-const app = express();
-const passport = require('passport');
-
-const path = require('path');
-const User = require('./models/users.model');
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs');
+// express 모듈 생성
+const express = require('express')
+// mongoose 모듈 생성
+const {default: mongoose} = require('mongoose');
 
 
+// express 모듈 변수 생성
+const app = express()
+
+// json read MiddleWare
 app.use(express.json());
-// html에서 form태그에서 보내주는 정보를 받아주는 미들웨어
-app.use(express.urlencoded( {extended: false}));
+// form read MiddleWare
+app.use(express.urlencoded({extended: false}));
 
-// 몽구스 db 연결하기
+// mongoose DB Connect url
 mongoose.connect(`mongodb+srv://son0925:1234@son0925.kwzwdli.mongodb.net/`)
-  .then(()=>{
-    console.log(`mongodb connected`)
+  .then(() => {
+    console.log(`Mongoose DB Connect`)
   })
   .catch((err) => {
-    console.log('err: ' +err)
+    console.log(err)
   })
-  
-app.use('/static', express.static(path.join(__dirname, 'public')))
 
 
-app.get('/login', (req,res) => {
-  res.render('login')
-})
-
-
-app.get('/signup', (req,res) => {
-  res.render('signup')
-})
-
-app.post('/signup', async(req,res) => {
-  // user 객체를 생성합니다
-  const user = new User(req.body) // email, password가 들어있음
-
-  // user 컬렉션에 유저를 저장합니다
-  try {
-    await user.save();
-    return res.status(200).json({
-      success: true,
-    })
-  } catch (error) {
-    console.log(error)
-  }
-})
-
+// 서버 실행
 const port = 4000;
-
-app.listen(port, ()=>{
-  console.log(`Listening on ${port}`);
+app.listen(port, () => {
+  console.log(`Running On Port ${port}`);
 })
-

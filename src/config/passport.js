04a56c23c5277f386a9551779fbd5fc61d47de2c
son0925 <shs00925@naver.com>
2024-04-30 23:35:42 +0900

    주석 오류 수정

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,20 +1,24 @@
 const passport = require('passport');
 const User = require('../models/users.model');
 const LocalStrategy = require('passport-local').Strategy;
+
+// local로그인 미들웨어
 passport.use(new LocalStrategy({usernameField: 'email', passwordField: 'password'},
   (email, password, done) => {
-    User.findOn({
+    // mongoose DB에서 문서를 찾는 함수 findOne()
+    User.findOne({
       email: email.toLocaleLowerCase()
     }, (err, user) => {
       if (err) return done(err);
 
       if (!user) return done(null, false, {msg: `Email ${email} not Found`});
-
+      // user는 db에서 찾은 pw, password는 클라이언트의 pw를 비교
       user.comparePassword(password, (err,isMatch) => {
         if (err) return done(err)
         if (isMatch){
           return done(null, user);
         }
+        // server.js의 authenticate에서 (err, user, info)로 받는 done
         return done(null, false, {msg: 'Invalid email or password.'});
       });
     })
